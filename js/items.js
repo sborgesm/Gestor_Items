747b0c42544709f1f12b82c5fd744b2cb60a7da4
Sandra Borges <106088439+sborgesm@users.noreply.github.com>
2024-12-03 17:20:18 +0100

    Revisión clases

--- js/items.js
+++ js/items.js
@@ -1,8 +1,8 @@
 class Items {
-    nom;
-    descripció;
-    data_creació;
-    data_modificació;
+    #nom;
+    #descripció;
+    #data_creació;
+    #data_modificació;
 
     constructor(nom, descripció, data_creació, data_modificació) {
         this.nom = nom;
@@ -10,4 +10,36 @@ class Items {
         this.data_creació = data_creació;
         this.data_modificació = data_modificació;
     }
+
+    setNom(nom) {
+        this.nom = nom;
+    }
+
+    getNom() {
+        return this.nom;
+    }
+
+    setDescripció(descripció) {
+        this.descripció = descripció;
+    }
+
+    getDescripció() {
+        return this.descripció;
+    }
+
+    setDataCreació(data_creació) {
+        this.data_creació = data_creació;
+    }
+
+    getDataCreació() {
+        return this.data_creació;
+    }
+
+    setDataModificació(data_modificació) {
+        this.data_modificació = data_modificació;
+    }
+
+    getDataModificació() {
+        return this.data_modificació;
+    }       
 }

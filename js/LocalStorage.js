ed57d1545f63506d405a874aa033a891cbbde651
Maarrcc <150380900+Maarrcc@users.noreply.github.com>
2024-12-03 17:40:52 +0100

    Update LocalStorage.js

--- js/LocalStorage.js
+++ js/LocalStorage.js
@@ -3,32 +3,32 @@
  * LocalStorage del navegador.
  */
 class LocalStorage {
-    /**
-     * Constructor de la classe.
-     * Inicialitza un array buit per a guardar els items.
-     */
-    constructor() {
-        this.items = [];
-    }
+  /**
+   * Constructor de la classe.
+   * Inicialitza un array buit per a guardar els items.
+   */
+  constructor() {
+    this.items = [];
+  }
 
-    /**
-     * Afegeix un item a l'array d'items i despres el guarda en el
-     * LocalStorage del navegador.
-     * @param {Object} item - item que es vol afegir.
-     */
-    afegirItem(item) {
-        this.items.push(item);
-        localStorage.setItem('items', JSON.stringify(this.items));
-    }
+  /**
+   * Afegeix un item a l'array d'items i despres el guarda en el
+   * LocalStorage del navegador.
+   * @param {Object} item - item que es vol afegir.
+   */
+  afegirItem(item) {
+    this.items.push(item);
+    localStorage.setItem("items", JSON.stringify(this.items));
+  }
 
-    /**
-     * Carrega els items guardats en el LocalStorage del navegador i els
-     * guarda en l'array d'items.
-     */
-    carregarItems() {
-        const itemsJSON = localStorage.getItem('items');
-        if (itemsJSON) {
-            this.items = JSON.parse(itemsJSON);
-        }
+  /**
+   * Mostra els items guardats en el LocalStorage del navegador.
+   */
+  mostrarItems() {
+    const items = JSON.parse(localStorage.getItem("items"));
+    if (items) {
+      this.items = items;
     }
+    return this.items;
+  }
 }

b426551d3b9c7ddbb7080529bfd734969302a41d
Sandra Borges <106088439+sborgesm@users.noreply.github.com>
2024-12-03 17:16:15 +0100

    Creación de clases

---
+++ js/Visuals.js
@@ -0,0 +1,5 @@
+class Visuals extends Items {
+    constructor(nom, descripció, data_creació, data_modificació) {
+        super(nom, descripció, data_creació, data_modificació);
+    }
+}
--- js/items.js
+++ js/items.js
@@ -1,3 +1,13 @@
-class Item {
-    imatge;
+class Items {
+    nom;
+    descripció;
+    data_creació;
+    data_modificació;
+
+    constructor(nom, descripció, data_creació, data_modificació) {
+        this.nom = nom;
+        this.descripció = descripció;
+        this.data_creació = data_creació;
+        this.data_modificació = data_modificació;
+    }
 }

2f033342edcdfae98b14c2fbfa55010e109da114
Sandra Borges <106088439+sborgesm@users.noreply.github.com>
2024-12-03 17:28:13 +0100

    Añadir imagen a la clase

--- js/Visuals.js
+++ js/Visuals.js
@@ -1,5 +1,16 @@
 class Visuals extends Items {
-    constructor(nom, descripció, data_creació, data_modificació) {
+    #url_imatge;
+
+    constructor(nom, descripció, data_creació, data_modificació, url_imatge) {
         super(nom, descripció, data_creació, data_modificació);
+        this.url_imatge = url_imatge;
+    }
+
+    setUrlImatge(url_imatge) {
+        this.url_imatge = url_imatge;
+    }
+
+    getUrlImatge() {
+        return this.url_imatge;
     }
 }

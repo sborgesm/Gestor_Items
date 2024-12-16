607965b2cca744cdaed24fdc24b65eca4a8695da
Maarrcc <150380900+Maarrcc@users.noreply.github.com>
2024-12-16 16:37:36 +0100

    Update targeta.js

--- js/modules/targeta.js
+++ js/modules/targeta.js
@@ -3,7 +3,20 @@ import { DivComponent } from "./div.js";
 import { Boto } from "./boto.js";
 import { Titol } from "./Titol.js";
 
+/**
+ * Classe que representa una targeta amb un titol, un element i un boto .
+ * La targeta es un component HTML que es pot renderitzar.
+ * @class
+ * @extends ComponentHTML
+ */
 class Targeta extends ComponentHTML {
+  /**
+   * Constructor de la classe Targeta.
+   * @param {string} classFill - La classe CSS del div que envolta la targeta.
+   * @param {string} nomBoto - El nom del boto de la targeta.
+   * @param {string} nomTitol - El nom del titol de la targeta.
+   * @param {ComponentHTML} element - L'element que es vol renderitzar dins de la targeta.
+   */
   constructor(classFill = "", nomBoto = "", nomTitol = "", element = null) {
     super("");
     this.div = new DivComponent("", classFill);
@@ -12,9 +25,13 @@ class Targeta extends ComponentHTML {
     this.element = element;
   }
 
+  /**
+   * Retorna l'element HTML de la targeta.
+   * @return {string} L'element HTML de la targeta.
+   */
   render() {
-    const elementHTML = this.element ? this.element.render() : "";
-    return `
+    const elementHTML = this.element ? this.element.render() : ""; // Si no hi ha element, retorna una string buit
+    return ` 
       <div class="${this.div.classNom}">
         ${this.titol.render()}
         ${elementHTML}
@@ -24,3 +41,4 @@ class Targeta extends ComponentHTML {
 }
 
 export { Targeta };
+

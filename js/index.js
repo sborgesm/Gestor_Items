8a3199435c2271112e33326d6c334ffb8f9793c5
Maarrcc <150380900+Maarrcc@users.noreply.github.com>
2025-01-09 16:44:08 +0100

    Actualizar els alerts i mostrar la informacio de la taula quan es borra un item

--- js/index.js
+++ js/index.js
@@ -4,132 +4,179 @@ import { Boto } from "./modules/boto.js";
 import { Titol } from "./modules/Titol.js";
 import { DivComponent } from "./modules/div.js";
 
-let nouDiv = new DivComponent('', 'gran', 'botons_inserir');
-let divTaula = new DivComponent('', 'gran', 'dades');
-let titolEntrada = new Titol('Taula d\'items', 'h1');// Creem instàncies dels divs i del títol
+let nouDiv = new DivComponent("", "gran", "botons_inserir");
+let divTaula = new DivComponent("", "gran", "dades");
+let titolEntrada = new Titol("Taula d'items", "h1"); // Creem instàncies dels divs i del títol
 
 let botoAfegir = new Boto("Clica per afegir Item", "boto-afegir");
 let botoCerca = new Boto("Cerca l'item", "boto-cerca");
 let boto10mes = new Boto("Clica per mostrar 10 resultats més", "boto-10mes");
-let botoMostrarItems = new Boto("Torna a mostrar els primers items", "boto-mostrar-items");// Creem instàncies dels botons necessaris
+let botoMostrarItems = new Boto(
+  "Torna a mostrar els primers items",
+  "boto-mostrar-items"
+); // Creem instàncies dels botons necessaris
 
-let inputCerca = new Input('InputCerca', 'text', '', "Introdueix el nom d'item");// Creem instància de l'input de cerca
+let inputCerca = new Input(
+  "InputCerca",
+  "text",
+  "",
+  "Introdueix el nom d'item"
+); // Creem instància de l'input de cerca
 
-let capcaleres = ['Nom item', 'Descripció', 'Data creacio', 'Data modificacio', 'Imatge relacionada', 'Esborrar item'];
+let capcaleres = [
+  "Nom item",
+  "Descripció",
+  "Data creacio",
+  "Data modificacio",
+  "Imatge relacionada",
+  "Esborrar item",
+];
 // Creem l'array amb les capçaleres de la taula
-let taula = new Taula(capcaleres, []);// Creem la instància de la taula
-taula.carregarDades();// Carreguem les dades a la taula
+let taula = new Taula(capcaleres, []); // Creem la instància de la taula
+taula.carregarDades(); // Carreguem les dades a la taula
 
-nouDiv.afegirFill(botoAfegir, inputCerca, botoCerca, boto10mes, botoMostrarItems);
+nouDiv.afegirFill(
+  botoAfegir,
+  inputCerca,
+  botoCerca,
+  boto10mes,
+  botoMostrarItems
+);
 // Afegim els fills al nouDiv
-divTaula.afegirFill(taula);// Afegim la taula al divTaula
+divTaula.afegirFill(taula); // Afegim la taula al divTaula
 
-document.addEventListener("DOMContentLoaded", () => {// Esperem que el DOM estigui llest i renderitzar
-    let contenedor = document.getElementById("divs");// Obtenim el div amb id "divs"
-    titolEntrada.append(contenedor);
-    nouDiv.append(contenedor);
-    divTaula.append(contenedor);// Afegim els fills al div amb id "divs"
+document.addEventListener("DOMContentLoaded", () => {
+  // Esperem que el DOM estigui llest i renderitzar
+  let contenedor = document.getElementById("divs"); // Obtenim el div amb id "divs"
+  titolEntrada.append(contenedor);
+  nouDiv.append(contenedor);
+  divTaula.append(contenedor); // Afegim els fills al div amb id "divs"
 
-    contenedor.addEventListener("click", (event) => {// Afegim funcio al div amb id "divs"
-        if (event.target.id && event.target.id.startsWith("boto-eliminar")) {// Si l'element té id i comença per "boto-eliminar"
-            let fila = event.target.closest("tr"); // Troba la fila més propera 
-            let itemNom = fila.cells[0].innerText; // Obté el nom de l'ítem de la primera cel·la
-            taula.eliminarItem(itemNom); // Elimina l'ítem de la taula
-            alert(itemNom + " esborrat"); // Mostra un missatge d'alerta
-            fila.remove(); // Elimina la fila de la taula
-        }else if (event.target.tagName === "TD") {// Si l'element és una cel·la
-            let fila = event.target.closest("tr");
-            let itemDades = Array.from(fila.cells).map((cell, index) => {
-                // Si és la columna de la imatge (index 4), agafem el `src` de l'element img
-                if (index === 4) {
-                    let img = cell.querySelector("img"); // Busca l'element <img> dins la cel·la
-                    return img ? img.src : ""; // Retorna el `src` o una cadena buida si no hi ha img
-                }
-                return cell.innerText; // Retorna el text per a altres cel·les
-            });
-
-        window.location.href = `./views/dadesItem.html?nom=${itemDades[0]}&descripcio=${itemDades[1]}&imatge=${encodeURIComponent(itemDades[4])}`;
-        // Construim l'URL amb les dades de l'ítem i redirigim a la pàgina de dades de l'ítem
+  contenedor.addEventListener("click", (event) => {
+    // Afegim funcio al div amb id "divs"
+    if (event.target.id && event.target.id.startsWith("boto-eliminar")) {
+      // Si l'element té id i comença per "boto-eliminar"
+      let fila = event.target.closest("tr"); // Troba la fila més propera
+      let itemNom = fila.cells[0].innerText; // Obté el nom de l'ítem de la primera cel·la
+      taula.eliminarItem(itemNom); // Elimina l'ítem de la taula
+      alert(itemNom + " esborrat"); // Mostra un missatge d'alerta
+      fila.remove(); // Elimina la fila de la taula
+    } else if (event.target.tagName === "TD") {
+      // Si l'element és una cel·la
+      let fila = event.target.closest("tr");
+      let itemDades = Array.from(fila.cells).map((cell, index) => {
+        // Si és la columna de la imatge (index 4), agafem el `src` de l'element img
+        if (index === 4) {
+          let img = cell.querySelector("img"); // Busca l'element <img> dins la cel·la
+          return img ? img.src : ""; // Retorna el `src` o una cadena buida si no hi ha img
         }
+        return cell.innerText; // Retorna el text per a altres cel·les
+      });
+
+      window.location.href = `./views/dadesItem.html?nom=${
+        itemDades[0]
+      }&descripcio=${itemDades[1]}&imatge=${encodeURIComponent(itemDades[4])}`;
+      // Construim l'URL amb les dades de l'ítem i redirigim a la pàgina de dades de l'ítem
+    }
+  });
+
+  botoAfegir.addEventListener("click", () => {
+    // Afegim funcio al botoAfegir
+    window.location.href = "./views/creacioItem.html"; // Redirigim a la pàgina de creació d'ítems
+  });
+
+  botoCerca.addEventListener("click", () => {
+    // Afegim funció al botoCerca
+    let text = document.getElementById("InputCerca").value.toLowerCase();
+    // Obtenim el text de l'input de cerca en minúscules
+    inputCerca.buidarInput(); // Buidem l'input de cerca
+
+    let dades = JSON.parse(localStorage.getItem("items")) || []; // Obtenim les dades de l'objecte localStorage
+
+    let itemsExactes = []; // Creem array per a guardar els items exactes
+    let itemsComencaAmb = []; // Creem array per a guardar els items que comencen amb el text
+    let itemsConte = new Set(); // Creem un Set per a guardar els items que contenen el text
+
+    dades.forEach((item) => {
+      let nom = item.nom ? item.nom.toLowerCase() : ""; // Obtenim el nom de l'item en minúscules
+      let descripcio = item.descripcio ? item.descripcio.toLowerCase() : ""; // Obtenim la descripció de l'item en minúscules
+
+      if (nom === text) {
+        itemsExactes.push(item); // Afegim l'item a l'array d'items exactes
+      } else if (nom.startsWith(text)) {
+        // Si el nom comença amb el text
+        itemsComencaAmb.push(item);
+      } else if (nom.includes(text) || descripcio.includes(text)) {
+        // Si el nom o la descripció contenen el text
+        itemsConte.add(item); // Afegim l'item al Set d'items que contenen el text
+      }
     });
 
-    botoAfegir.addEventListener("click", () => {// Afegim funcio al botoAfegir
-        window.location.href = "./views/creacioItem.html";// Redirigim a la pàgina de creació d'ítems
-    });
-
-    botoCerca.addEventListener("click", () => { // Afegim funció al botoCerca
-        let text = document.getElementById("InputCerca").value.toLowerCase();
-        // Obtenim el text de l'input de cerca en minúscules
-        inputCerca.buidarInput(); // Buidem l'input de cerca
+    let trobat = false; // Variable per controlar si s'ha trobat algun item
     
-        let dades = JSON.parse(localStorage.getItem("items")) || []; // Obtenim les dades de l'objecte localStorage
-    
-        let itemsExactes = []; // Creem array per a guardar els items exactes
-        let itemsComencaAmb = []; // Creem array per a guardar els items que comencen amb el text
-        let itemsConte = new Set(); // Creem un Set per a guardar els items que contenen el text
-    
-        dades.forEach(item => {
-            let nom = item.nom ? item.nom.toLowerCase() : ""; // Obtenim el nom de l'item en minúscules
-            let descripcio = item.descripcio ? item.descripcio.toLowerCase() : ""; // Obtenim la descripció de l'item en minúscules
-    
-            if (nom === text) {
-                itemsExactes.push(item);// Afegim l'item a l'array d'items exactes
-                alert("S'ha trobat l'item");
-            } else if (nom.startsWith(text)) {// Si el nom comença amb el text
-                itemsComencaAmb.push(item);
-                alert("S'ha trobat item que té un nom comença amb el text");
-            } else if (nom.includes(text) || descripcio.includes(text)) {// Si el nom o la descripció contenen el text
-                itemsConte.add(item);// Afegim l'item al Set d'items que contenen el text
-                alert("S'ha trobat item que té el text a dins del nom o la descripció");
-            }
-        });
-    
-        let itemsFiltrats = [...itemsExactes, ...itemsComencaAmb, ...itemsConte];// Creem un array amb tots els items filtrats
-    
-        if (itemsFiltrats.length > 0) {
-            taula.carregarDades(itemsFiltrats); // Actualitzar la taula amb els items filtrats
-            taula.update(document.getElementById("dades")); // Actualitzar la taula a la vista
-        } else {
-            alert("No s'han trobat coincidències.");
-            taula.carregarDades([]); // Assegurar que la taula es buida
-        }
-    });
-    
+    // Mostrar alerts segons els resultats de la cerca
+    if (itemsExactes.length > 0 && trobat === false) {
+      alert("S'ha trobat l'item");
+      trobat = true;
+    }
+    if (itemsComencaAmb.length > 0 && trobat === false) {
+      alert("S'ha trobat un item que comença amb el text");
+      trobat = true;
+    }
+    if (itemsConte.size > 0 && trobat === false) {
+      alert(
+        "S'ha trobat un item que conte el text a dins del nom o la descripció"
+      );
+      trobat = true;
+    }
 
-    let vegadesClicat = 0; // Definim fora del manejador per mantenir el valor de les vegades que s'ha clicat
+    let itemsFiltrats = [...itemsExactes, ...itemsComencaAmb, ...itemsConte]; // Creem un array amb tots els items filtrats
 
-    boto10mes.addEventListener("click", () => {
-        let dades = JSON.parse(localStorage.getItem("items")) || [];
-        vegadesClicat++; // Incrementem el comptador de clics
+    if (itemsFiltrats.length > 0) {
+      taula.carregarDades(itemsFiltrats); // Actualitzar la taula amb els items filtrats
+      taula.update(document.getElementById("dades")); // Actualitzar la taula a la vista
+    } else {
+      alert("No s'han trobat coincidències.");
+      taula.carregarDades([]); // Assegurar que la taula es buida
+    }
+  });
 
-        if (dades.length === 0) {// Si no hi ha dades per mostrar
-            alert("No hi ha més items per carregar");
-            return;
-        }
+  let vegadesClicat = 0; // Definim fora del manejador per mantenir el valor de les vegades que s'ha clicat
 
-        let inici = vegadesClicat * 10;
-        // Calculem l'inici de les dades a mostrar multiplicant per 10 el nombre de clics
-        let fi = inici + 10;// Calculem el final de les dades a mostrar sumant 10 a l'inici
-        let dadesPerMostrar = dades.slice(inici, fi);
-        // Obtenim les dades a mostrar amb el mètode slice amb els paràmetres inici i fi
+  boto10mes.addEventListener("click", () => {
+    let dades = JSON.parse(localStorage.getItem("items")) || [];
+    vegadesClicat++; // Incrementem el comptador de clics
 
-        if (dadesPerMostrar.length === 0) {// Si no hi ha més dades per mostrar
-            alert("No hi ha més items per carregar");// Mostrem un missatge d'alerta
-            return;
-        }
+    if (dades.length === 0) {
+      // Si no hi ha dades per mostrar
+      alert("No hi ha més items per carregar");
+      return;
+    }
 
-        taula.carregarDades(dadesPerMostrar); // Actualitzar la taula amb els nous items
-        taula.update(document.getElementById("dades")); // Actualitzar la taula a la vista
+    let inici = vegadesClicat * 10;
+    // Calculem l'inici de les dades a mostrar multiplicant per 10 el nombre de clics
+    let fi = inici + 10; // Calculem el final de les dades a mostrar sumant 10 a l'inici
+    let dadesPerMostrar = dades.slice(inici, fi);
+    // Obtenim les dades a mostrar amb el mètode slice amb els paràmetres inici i fi
 
-        alert(`S'han carregat ${dadesPerMostrar.length} items més`);
-        // Mostrem un missatge amb el nombre d'items carregats
-    });
+    if (dadesPerMostrar.length === 0) {
+      // Si no hi ha més dades per mostrar
+      alert("No hi ha més items per carregar"); // Mostrem un missatge d'alerta
+      return;
+    }
 
-    botoMostrarItems.addEventListener("click", () => {// Afegim funcionalitat al botoMostrarItems
-        let dades = JSON.parse(localStorage.getItem("items")) || [];
-        taula.carregarDades(dades); // Carregar els 10 primers items
-        taula.update(document.getElementById("dades")); // Actualitzar la taula a la vista
-        alert("S'han tornat a mostrar els primers 10 items");
-    });
+    taula.carregarDades(dadesPerMostrar); // Actualitzar la taula amb els nous items
+    taula.update(document.getElementById("dades")); // Actualitzar la taula a la vista
+
+    alert(`S'han carregat ${dadesPerMostrar.length} items més`);
+    // Mostrem un missatge amb el nombre d'items carregats
+  });
+
+  botoMostrarItems.addEventListener("click", () => {
+    // Afegim funcionalitat al botoMostrarItems
+    let dades = JSON.parse(localStorage.getItem("items")) || [];
+    taula.carregarDades(dades); // Carregar els 10 primers items
+    taula.update(document.getElementById("dades")); // Actualitzar la taula a la vista
+    alert("S'han tornat a mostrar els primers 10 items");
+  });
 });
-
--- js/modules/taula.js
+++ js/modules/taula.js
@@ -15,10 +15,10 @@ class Taula extends ComponentHTML {
    * La taula es pot actualitzar amb noves dades amb el metode update.
    */
   constructor(capcaleres, dada) {
-    let html = Taula.crearTaulaHTML(capcaleres, dada);// Crear l'HTML de la taula
-    super(html);// Cridem al constructor de la classe pare
-    this.capcaleres = capcaleres;// Atribut per guardar les capcaleres
-    this.dada = dada;// Atribut per guardar les dades
+    let html = Taula.crearTaulaHTML(capcaleres, dada); // Crear l'HTML de la taula
+    super(html); // Cridem al constructor de la classe pare
+    this.capcaleres = capcaleres; // Atribut per guardar les capcaleres
+    this.dada = dada; // Atribut per guardar les dades
   }
 
   /**
@@ -30,65 +30,78 @@ class Taula extends ComponentHTML {
    * @returns {String} l'HTML de la taula
    */
   static crearTaulaHTML(capcaleres, dada) {
-    let taulaHTML = '<table><thead><tr>';// Inicialitzar l'HTML de la taula
+    let taulaHTML = "<table><thead><tr>"; // Inicialitzar l'HTML de la taula
 
     // Afegir capçaleres
-    capcaleres.forEach((capcalera) => {// Recorrer les capcaleres
-      taulaHTML += `<th>${capcalera}</th>`;// Afegim cada capçalera a l'HTML
+    capcaleres.forEach((capcalera) => {
+      // Recorrer les capcaleres
+      taulaHTML += `<th>${capcalera}</th>`; // Afegim cada capçalera a l'HTML
     });
-    taulaHTML += "</tr></thead><tbody>";// Tancar la fila de capçaleres i obrir el cos de la taula
+    taulaHTML += "</tr></thead><tbody>"; // Tancar la fila de capçaleres i obrir el cos de la taula
 
     // Afegir dades
-    dada.forEach((fila) => {// Recorrem les dades
-      taulaHTML += "<tr>";// Obrim una fila
-      fila.forEach((cell) => {// Recorrem les cel·les de la fila
-        taulaHTML += `<td>${cell}</td>`;// Afegim cada cel·la a l'HTML
+    dada.forEach((fila) => {
+      // Recorrem les dades
+      taulaHTML += "<tr>"; // Obrim una fila
+      fila.forEach((cell) => {
+        // Recorrem les cel·les de la fila
+        taulaHTML += `<td>${cell}</td>`; // Afegim cada cel·la a l'HTML
       });
-      taulaHTML += "</tr>";// Tanquem la fila
+      taulaHTML += "</tr>"; // Tanquem la fila
     });
 
-    taulaHTML += "</tbody></table>";// Afegim el tanquament del cos de la taula
+    taulaHTML += "</tbody></table>"; // Afegim el tanquament del cos de la taula
     return taulaHTML; // Retornem l'HTML de la taula
   }
 
   //Metode per carregar les dades de l'objecte localStorage i mostrar-les a la taula.
-  
-  carregarDades(dadesFiltrades = null) {// Carregar les dades amb les dades filtrades per defecte a null
-  let dades = dadesFiltrades || JSON.parse(localStorage.getItem("items") || "[]");
-  // Obtenir les dades filtrades o de l'objecte localStorage o un array buit si no hi ha dades
-  let imatgeEliminar = new Imatge("./img/brossa.png", " ", "50", "50", "boto-eliminar");
-  // Crear una instància de la classe Imatge amb la imatge de la paperera
-  if (dades.length > 10) {// Si hi ha més de 10 items a les dades
-    dades = dades.slice(0, 10); 
-    // Mostrem només els 10 primers items amb el mètode slice on el primer paràmetre és l'índex inicial i el segon l'índex final
-  };
-  
-  // Cream les files amb les dades
-  let files = dades.map((dada) => [// Recorrem les dades i retornem un array amb les dades de cada item
-    dada.nom,
-    dada.descripcio,
-    dada.dataCreacio || "-",
-    dada.dataModificacio || "-",
-    dada.imatge || "-",
-    imatgeEliminar.html,
-  ]);
 
-  this.posarDada(files); // Actualitzar la taula amb les dades (ordenades)
-}
+  carregarDades(dadesFiltrades = null) {
+    // Carregar les dades amb les dades filtrades per defecte a null
+    let dades =
+      dadesFiltrades || JSON.parse(localStorage.getItem("items") || "[]");
+    // Obtenir les dades filtrades o de l'objecte localStorage o un array buit si no hi ha dades
+    let imatgeEliminar = new Imatge(
+      "./img/brossa.png",
+      " ",
+      "50",
+      "50",
+      "boto-eliminar"
+    );
+    // Crear una instància de la classe Imatge amb la imatge de la paperera
+    if (dades.length > 10) {
+      // Si hi ha més de 10 items a les dades
+      dades = dades.slice(0, 10);
+      // Mostrem només els 10 primers items amb el mètode slice on el primer paràmetre és l'índex inicial i el segon l'índex final
+    }
 
+    // Cream les files amb les dades
+    let files = dades.map((dada) => [
+      // Recorrem les dades i retornem un array amb les dades de cada item
+      dada.nom,
+      dada.descripcio,
+      dada.dataCreacio || "-",
+      dada.dataModificacio || "-",
+      dada.imatge || "-",
+      imatgeEliminar.html,
+    ]);
+
+    this.posarDada(files); // Actualitzar la taula amb les dades (ordenades)
+  }
 
   /**
    * Metode per eliminar un item de la taula.
    * @param {string} nom - l'id de l'item a eliminar
    */
   eliminarItem(nom) {
-      let dades = JSON.parse(localStorage.getItem("items") || "[]");
-      dades = dades.filter(item => item.nom !== nom); // Filtrar els items per eliminar l'item amb l'id donat
+    let dades = JSON.parse(localStorage.getItem("items") || "[]");
+    dades = dades.filter((item) => item.nom !== nom); // Filtrar els items per eliminar l'item amb l'id donat
 
-      localStorage.setItem("items", JSON.stringify(dades)); // Actualitzar l'objecte localStorage
+    localStorage.setItem("items", JSON.stringify(dades)); // Actualitzar l'objecte localStorage
 
-      this.carregarDades( ); // Actualitzar la taula
-    }
+    this.carregarDades(); // Actualitzar la taula
+    this.update(document.getElementById("dades")); // Actualiza el DOM
+  }
 
   /**
    * Mètode per canviar les capcaleres.
@@ -97,10 +110,10 @@ class Taula extends ComponentHTML {
    * @param {Array} novesCapcaleres - les noves capcaleres de la taula
    */
   posarCapcaleres(novesCapcaleres) {
-      this.capcaleres = novesCapcaleres;// Actualitzar les capçaleres
-      this.html = Taula.crearTaulaHTML(this.capcaleres, this.dada);
-      // Actualitzar l'HTML de la taula amb les noves capçaleres
-    }
+    this.capcaleres = novesCapcaleres; // Actualitzar les capçaleres
+    this.html = Taula.crearTaulaHTML(this.capcaleres, this.dada);
+    // Actualitzar l'HTML de la taula amb les noves capçaleres
+  }
 
   /**
    * Mètode per canviar les dades.
@@ -109,9 +122,9 @@ class Taula extends ComponentHTML {
    * @param {Array} novaDada - les noves dades de la taula
    */
   posarDada(novaDada) {
-      this.dada = novaDada;
-      this.html = Taula.crearTaulaHTML(this.capcaleres, this.dada);
-    }
+    this.dada = novaDada;
+    this.html = Taula.crearTaulaHTML(this.capcaleres, this.dada);
+  }
 }
 
 export { Taula };
